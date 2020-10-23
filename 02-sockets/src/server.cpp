f7fd7d8ec9ef2771af8038c3019e671c654ac767
Brad McDanel <mcdabw6@gmail.com>
2020-10-23 10:18:31 -0400

    Working on Programming Assignment 2 release.

--- 02-sockets/src/server.cpp
+++ 02-sockets/src/server.cpp
@@ -1,7 +1,10 @@
+#include <inttypes.h>
 #include <asio.hpp>
 #include <ctime>
 #include <iostream>
 #include <string>
+
+#include "bakery.hpp"
 
 using asio::ip::tcp;
 
@@ -9,30 +12,35 @@ int main() {
   asio::io_context io_context;
   tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 3000));
 
-  int counter = 0;
+  // Use this bakery to handle queries from the client
+  Bakery bakery = text_deserializer("../data/bakery.txt");
+
+  uint16_t counter = 0;
 
   while (true) {
-    // Wait for client (acceptor.accept is blocking)
+    // Wait for client
     std::cout << "Blocked for read" << std::endl;
     tcp::socket socket(io_context);
     acceptor.accept(socket);
 
-    // Create a buf and read into it
-    std::array<char, 128> buf;
+    std::array<uint8_t, 3> buf;
     asio::error_code error;
     size_t len = socket.read_some(asio::buffer(buf), error);
-
-    // Get the first byte from buf, convert to int, and add to counter
-    int x = int(buf[0]);
-    counter += x;
-
-    // Convert counter to std::string message and send back
-    std::string message = std::to_string(counter) + "\n";
-    asio::write(socket, asio::buffer(message), error);
 
     // Example of error handling
     // if (error != asio::error::eof)
     //   throw asio::system_error(error);
+
+    // Add x to counter
+    auto x = uint8_t(buf[0]);
+    counter += x;
+    std::cout << +x << " " << counter << std::endl;
+
+    buf.fill(0);
+
+    std::memcpy(&buf, &counter, sizeof(uint16_t));
+
+    asio::write(socket, asio::buffer(buf), error);
   }
 
   return 0;
