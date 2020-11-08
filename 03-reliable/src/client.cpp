1db10c8a851e79dd12ab96657aaafa01a1dbbfeb
llliting <hllliting@gmail.com>
2020-11-07 19:27:40 -0800

    tryy

--- 03-reliable/src/client.cpp
+++ 03-reliable/src/client.cpp
@@ -1,5 +1,7 @@
 #include <asio.hpp>
 #include <iostream>
+#include <queue>
+
 #include "../include/sender.hpp"
 
 using asio::ip::tcp;
@@ -18,12 +20,42 @@ int main() {
   // ./server --no-delay --no-packet-drops which makes the below code work
 
   // As an example, you could start by requesting the first 10 messages
+  std::string poem = "";
   int32_t curr_msg = 0;
-  for (int i = 0; i < 10; i++) {
+  std::queue <int32_t> q;
+  for (int i = 0; i < 852; i+=2) {
+    //std::cout << "semt " << std::endl;
     sender.request_msg(i);
-    curr_msg++;
+    sender.request_msg(i+1);
+    q.push(i);
+    q.push(i+1);
+    curr_msg+=2;
+    if(sender.data_ready()){
+      auto msg0 = sender.get_msg();
+      auto msg1 = sender.get_msg();
+      if(q.front() == msg0.msg_id){
+        auto data_str0 = std::string(msg0.data.data(), CHUNK_SIZE);
+        auto data_str1 = std::string(msg1.data.data(), CHUNK_SIZE);
+        poem += data_str0 + data_str1;
+        std::cout << "msg_id(" << msg0.msg_id << ")::" << data_str0 << std::endl;
+        std::cout << "msg_id(" << msg1.msg_id << ")::" << data_str1 << std::endl;
+      }
+      else{
+        auto data_str0 = std::string(msg1.data.data(), CHUNK_SIZE);
+        auto data_str1 = std::string(msg0.data.data(), CHUNK_SIZE);
+        poem += data_str0 + data_str1;
+        std::cout << "msg_id(" << msg1.msg_id << ")::" << data_str0 << std::endl;
+        std::cout << "msg_id(" << msg0.msg_id << ")::" << data_str1 << std::endl;
+      }
+
+      
+
+      
+    }
   }
 
+
+/*
   while (true) {
     if (sender.data_ready()) {
       // Get a response Msg:
@@ -38,6 +70,7 @@ int main() {
       std::cout << "msg_id(" << msg.msg_id << ")::" << data_str << std::endl;
     }
   }
+  */
 
   return 0;
 }
